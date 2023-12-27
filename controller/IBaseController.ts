5ddd13679f81dbf1a2ff61b3ee49191f872b32b7
thanh-ukpc <thanh.do@ukparkingcontrol.com>
2023-12-27 14:58:51 +0700

    updaate base Model

--- controller/IBaseController.ts
+++ controller/IBaseController.ts
@@ -1,12 +1,13 @@
-import { CountProps, FindProps, ListProps, Paging } from '@Core';
+import { Paging } from '../Paging';
+import { CountProps, FindProps, ListProps } from '../Query';
 
 export interface IBaseController<T> {
-    get(id: string): Promise<T | undefined>;
-    upsert(t: Partial<T>): Promise<T>;
-    delete(id: string): Promise<T | undefined>;
-    list(props: ListProps<T>): Promise<Paging<T>>;
-    find(props: FindProps<T>): Promise<T[]>;
-    findOne(props: Omit<FindProps<T>, 'limit'>): Promise<T>;
-    count(props: CountProps<T>): Promise<number>;
-    bulkUpsert(props: { list: T[] }): Promise<T[]>;
+  get(id: string): Promise<T | undefined>;
+  upsert(t: Partial<T>): Promise<T>;
+  delete(id: string): Promise<T | undefined>;
+  list(props: ListProps<T>): Promise<Paging<T>>;
+  find(props: FindProps<T>): Promise<T[]>;
+  findOne(props: Omit<FindProps<T>, 'limit'>): Promise<T>;
+  count(props: CountProps<T>): Promise<number>;
+  bulkUpsert(props: { list: T[] }): Promise<T[]>;
 }
